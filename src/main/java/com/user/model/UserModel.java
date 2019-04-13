29f249d482529f1b6d9e553b8b08e67b6ba49681
tom_glb <goodMorning_glb@atguigu.com>
2019-04-13 22:31:13 +0800

    web 工程

---
+++ src/main/java/com/user/model/UserModel.java
@@ -0,0 +1,19 @@
+package com.user.model;
+
+public class UserModel {
+	private String name;
+
+	public UserModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
